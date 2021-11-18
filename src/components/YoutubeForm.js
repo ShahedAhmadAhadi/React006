c279f32caa60884ce1e51afdb191a7e84535243a
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-18 11:06:17 +0430

    all validations and formik object passed

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,20 +1,53 @@
 import React from 'react'
 import { useFormik } from 'formik'
 
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+const onSubmit = values => {
+    console.log(values)
+}
+
+const validate = values => {
+    let errors = {}
+
+    if (!values.name) {
+        return errors.name = "Required"
+    }
+    if (!values.email) {
+        return errors.email = "Required"
+    }else if (!/^A-Z0-9._%+-]+@[A-Z0.9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email format'
+    }
+    if (!values.channel) {
+        return errors.channel = "Required"
+    }
+
+    return errors
+}
+
 function YoutubeForm() {
 
-    const formik = useFormik({})
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate 
+    })
+
+    console.log(formik.values)
 
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
+                <input type="text" name="name" id="name" onChange={formik.handleChange} value={formik.values.name} />
                 <label htmlFor="email">E-mail</label>
-                <input type="email" name="email" id="email" />
+                <input type="email" name="email" id="email" onChange={formik.handleChange} value={formik.values.email} />
                 <label htmlFor="channel">Channel</label>
-                <input type="text" name="channel" id="channel" />
-                <button>Submit</button>
+                <input type="text" name="channel" id="channel" onChange={formik.handleChange} value={formik.values.channel} />
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
