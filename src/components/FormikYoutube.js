c9c8347acedc23f6f7c9f345776569138c2221be
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-20 21:46:58 +0430

    all component changed to render props to get manual trigers of errors:

--- src/components/FormikYoutube.js
+++ src/components/FormikYoutube.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik'
 import * as Yup from 'yup'
 import TextError from './TextError'
 
@@ -8,7 +8,8 @@ const initialValues = {
     email: '',
     channel: '',
     comment: '',
-    phoneNumbers: ['', '']
+    phoneNumbers: ['', ''],
+    phNumbers: ['']
 }
 const onSubmit = values => {
     console.log(values)
@@ -20,11 +21,22 @@ const validationSchema = Yup.object({
     channel: Yup.string().required('Required!')
 })
 
+const validateComments = value => {
+    let error
+    if (!value){
+        error = "Required"
+    }
+    return error
+}
+
 function FormikYoutube() {
 
     return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-            <Form>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} validateOnChange={false}>
+            {
+                formik => {
+                    return (
+<Form>
                 <div className="form-control">
                     <label htmlFor="name">Name</label>
                     <Field type="text" name="name" id="name" />
@@ -46,7 +58,8 @@ function FormikYoutube() {
                 </div>
                 <div className="form-control">
                     <label htmlFor="comment">Comment</label>
-                    <Field as="textarea" name="comment" id="comment" />
+                    <Field as="textarea" name="comment" id="comment" validate={validateComments} />
+                    <ErrorMessage name="comment" component={TextError} />
                 </div>
                 <div className="form-control">
                     <label htmlFor="primaryPh">Primary phone number</label>
@@ -56,8 +69,33 @@ function FormikYoutube() {
                     <label htmlFor="secondaryPh">Secondary phone number</label>
                     <Field name="phoneNumbers[1]" id="secondaryPh" />
                 </div>
+                <FieldArray name="phNumbers">
+                    {
+                        fieldArrayProps => {
+                            console.log('fieldProps', fieldArrayProps)
+                            const {push, remove, form} = fieldArrayProps
+                            const { values } = form
+                            const { phNumbers } = values
+                            return (
+                                <div>
+                                    {phNumbers.map((phNumbers, index) => (
+                                        <div key={index}>
+                                            <Field name={`phNumbers[${index}]`} />
+                                            {index > 0 && <button type="button" onClick={() => remove(index)}> - </button>}
+                                            <button type="button" onClick={() => push('')}> + </button>
+                                        </div>
+                                    ))}
+                                </div>
+                            )
+                        }
+                    }
+                </FieldArray>
+                <button type="button" onClick={() => formik.setTouched({name: true, channel: true, email: true, comment: true})}>validate</button>
                 <button type="submit">Submit</button>
             </Form>
+                    )
+                }
+            }
         </Formik>
     )
 }
