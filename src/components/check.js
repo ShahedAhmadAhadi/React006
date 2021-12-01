7827662dde54fbf48cff1945e22cec82019919dd
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-01 23:54:30 +0430

    new pacakge added and used for check

--- src/components/check.js
+++ src/components/check.js
@@ -1,10 +1,18 @@
-import React from 'react'
-import CheckBox from '@mui/icons-material/CheckBox';
+import React, {useState} from 'react'
+import Checkbox from '@mui/material/Checkbox'
+import { Face, Facebook } from '@mui/icons-material'
+import { Rating } from '@mui/material'
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+
 
 function Check() {
+    const [value, setValue] = useState(2)
     return (
         <div>
-            <CheckBox />
+            <Checkbox icon={<Facebook />} checkedIcon={<Facebook />} />
+            <Rating value={value} onChange={(event, newValue) => {
+                setValue(newValue)
+            }} />
         </div>
     )
 }
