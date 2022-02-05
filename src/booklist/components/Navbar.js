b15a20b4c844081bd6d5521cd9ea454b7cf9a675
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-05 11:58:57 +0430

    Bugfix

--- src/booklist/components/Navbar.js
+++ src/booklist/components/Navbar.js
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react'
-import { BookContext } from '../context/BookContext'
+import React, { useContext } from 'react';
+import { BookContext } from '../context/BookContext';
 
 const Navbar = () => {
-    const { books } = useContext(BookContext)
+    const { books } = useContext(BookContext);
     return (
-        <div className='navbar'>
+        <div className="navbar">
             <h1>Reading List</h1>
             <p>{books.length}</p>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
--- src/booklist/context/BookContext.js
+++ src/booklist/context/BookContext.js
@@ -1,22 +1,25 @@
-import React, { createContext, useState } from 'react'
-import { v1 as uuid } from 'uuid'
+import React, { createContext, useState } from 'react';
+import { v1 as uuid } from 'uuid';
 
-export const BookContext = createContext()
+export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([{title: 'name of the wind', author: 'patrick rothfoss', id: 1}, {title: 'the final empire', author: 'brandon sanderson', id: 2}])
+    const [books, setBooks] = useState([
+        { title: 'name of the wind', author: 'patrick rothfoss', id: 1 },
+        { title: 'the final empire', author: 'brandon sanderson', id: 2 },
+    ]);
     const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuid()}])
-    }
+        setBooks([...books, { title, author, id: uuid() }]);
+    };
     const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id));
-      }
+        setBooks(books.filter((book) => book.id !== id));
+    };
 
     return (
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{ books, addBook, removeBook }}>
             {props.children}
         </BookContext.Provider>
-    )
-}
+    );
+};
 
-export default BookContextProvider
+export default BookContextProvider;
