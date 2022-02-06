3e99b9408cfac8215dabc428dcb24dbb626c0217
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-06 11:38:57 +0430

    Bugfix

--- src/booklist/components/BookDetails.js
+++ src/booklist/components/BookDetails.js
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react'
-import { BookContext } from '../context/BookContext'
+import React, { useContext } from 'react';
+import { BookContext } from '../context/BookContext';
 
-const BookDetails = ({book, id}) => {
-    const {removeBook} = useContext(BookContext)
+const BookDetails = ({ book, id }) => {
+    const { removeBook } = useContext(BookContext);
     return (
         <li onClick={() => removeBook(book.id)} key={id}>
-            <div className='title'>{book.title}</div>
-            <div className='author'>{book.author}</div>
+            <div className="title">{book.title}</div>
+            <div className="author">{book.author}</div>
         </li>
-    )
-}
+    );
+};
 
-export default BookDetails
+export default BookDetails;
