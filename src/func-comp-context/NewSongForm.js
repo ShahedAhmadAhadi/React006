c5cd6a43ea2944e142950f1a29ec6b6a2833b97b
Shahed Ahmad <shaheda.a16@gmail.com>
2022-02-06 11:40:00 +0430

    func-components edited and debugged

--- src/func-comp-context/NewSongForm.js
+++ src/func-comp-context/NewSongForm.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const NewSongForm = ({ addSong }) => {
-    const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
-        addSong(title)
-        setTitle('')
-    }
+        addSong(title);
+        setTitle('');
+    };
     return (
         <form onSubmit={handleSubmit}>
             <label>Song name:</label>
-            <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input
+                type="text"
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
             <input type="submit" value="Add Song" />
         </form>
-    )
-}
+    );
+};
 
-export default NewSongForm
+export default NewSongForm;
--- src/func-comp-context/SongList.js
+++ src/func-comp-context/SongList.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { v1 as uuid } from 'uuid'
-import NewSongForm from './NewSongForm'
+import React, { useState, useEffect } from 'react';
+import { v1 as uuid } from 'uuid';
+import NewSongForm from './NewSongForm';
 
 const SongList = () => {
-    const [songs, setSongs] = useState([{ title: 'almost home', id: 1 }, { title: 'memory gospel', id: 2 }, { title: 'this wild darkness', id: 3 }])
+    const [songs, setSongs] = useState([
+        { title: 'almost home', id: 1 },
+        { title: 'memory gospel', id: 2 },
+        { title: 'this wild darkness', id: 3 },
+    ]);
     const addSong = (title) => {
-        setSongs([...songs, { title, id: uuid() }])
-    }
-    useEffect(() => {
-
-    }, [])
+        setSongs([...songs, { title, id: uuid() }]);
+    };
+    useEffect(() => {}, []);
     return (
-        <div className='song-list'>
+        <div className="song-list">
             <ul>
                 {songs.map((song) => {
-                    return (
-                        <li key={song.id}>{song.title}</li>
-                    )
+                    return <li key={song.id}>{song.title}</li>;
                 })}
             </ul>
             <NewSongForm addSong={addSong} />
         </div>
-    )
-}
+    );
+};
 
-export default SongList
+export default SongList;
